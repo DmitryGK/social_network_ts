a7f64379ae461c94377da3df955bd5383a3fa935
DmitryGK <gulyakevichdmitry@gmail.com>
2022-01-13 04:36:07 +0300

    css

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import p from './Profile.module.css'
-import pic from '../../pictures/wp.jpg'
+import pic from '../../pictures/richard-horvath-RAZU_R66vUc-unsplash.jpg'
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
