e28b77a0786fd80ad059ac29e41bc8bc9a15c7c5
DmitryGK <gulyakevichdmitry@gmail.com>
2022-04-05 10:26:13 +0300

    login page add

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
- const Login = () => {
+const Login = () => {
 
     return (
-        <h1>LOGIN</h1>
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <input placeholder={'Login'}/>
+                </div>
+                <div>
+                <input placeholder={'Password'}/>
+                </div>
+                <div>
+                    <input type={'checkbox'}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </div>
     )
 
 }
+
+
 
 export default Login
