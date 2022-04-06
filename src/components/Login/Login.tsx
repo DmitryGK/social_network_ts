4748112cf364b27db3daf5b2a3c9314011d597a2
DmitryGK <gulyakevichdmitry@gmail.com>
2022-04-06 10:27:01 +0300

    add: fields redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, InjectedFormProps, reduxForm } from 'redux-form';
 
-const LoginForm = () => {
 
-    return (  
-            <form>
-                <div>
-                    <input placeholder={'Login'}/>
-                </div>
-                <div>
-                <input placeholder={'Password'}/>
-                </div>
-                <div>
-                    <input type={'checkbox'}/> remember me
-                </div>
-                <div>
-                    <button>Login</button>
-                </div>
-            </form>
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'} />
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'} />
+            </div>
+            <div>
+                <Field component={'input'} name={'rememberMe'} type={'checkbox'} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 
 }
@@ -31,11 +38,11 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
-            <LoginReduxForm/>
+            <LoginReduxForm />
         </div>
     )
 }
 
 
 
-export default Login
+export default Login 
